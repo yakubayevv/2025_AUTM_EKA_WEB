b1f3e4ae14d4939704ac21972627c44f59f17755
Fabian Saur <fabiansaur@gmx.de>
2025-11-12 13:57:13 +0200

    3D Game

--- FabianS/3D/js/script.js
+++ FabianS/3D/js/script.js
@@ -7,6 +7,41 @@
 
 var world = document.getElementById("world");
 
+
+let mySquares = []
+
+let squares = [
+    // TranslateVal1, TranslateVal2, TranslateVal3, , , , width, height, color, opacity
+    [500, 300, 100, 0, 0, 0, 200, 200, "blueviolet", 0.5],
+    
+    [500, 300, -100, 0, 0, 0, 200, 200, "yellowgreen", 0.5],
+    
+    [500, 300, -100, 0, 0, 0, 200, 200, "black", 0.5],
+]
+
+
+function drawWorld(){
+
+    let mySquare1
+    for(let counter = 0; counter < squares.length; counter++){
+        mySquare1 = document.createElement("div")
+        mySquare1.id = `square${counter}`
+        mySquare1.style.position = "absolute"
+        mySquare1.style.width = `${squares[counter][6]} px`
+        mySquare1.style.height = `${squares[counter][7]} px`
+        mySquare1.style.background = `${squares[counter][8]} px`
+        mySquare1.style.opacity = `${squares[counter][9]} px`
+        mySquare1.style.transform = `
+            translate3d(${squares[counter][0]} px, ${squares[counter][1]} px, ${squares[counter][2]} px) 
+            rotateX(${squares[counter][3]} deg)
+            rotateY(${squares[counter][4]} deg)
+            rotateZ(${squares[counter][5]} deg)
+        `
+        world.appendChild(mySquare1)
+        mySquares.push(mySquare1)
+    }
+
+}
 let drx = 0;
 let dry = 0
 let drz = 0;
